7e4a8989afb5801dc4c9020ea0a91356b28a3f4f
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-04-08 16:48:56 +0530

    build issue fix

--- vitals/Main.java
+++ vitals/Main.java
@@ -2,23 +2,14 @@
 package vitals;
 
 public class Main {
-    static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(temperature < 0 || temperature > 45) {
-            System.out.println("Temperature is out of range!");
-            return false;
-        } else if(soc < 20 || soc > 80) {
-            System.out.println("State of Charge is out of range!");
-            return false;
-        } else if(chargeRate > 0.8) {
-            System.out.println("Charge Rate is out of range!");
-            return false;
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        assert(batteryIsOk(25, 70, 0.7f) == true);
-        assert(batteryIsOk(50, 85, 0.0f) == false);
+   public static void main(String[] args) {
+		
+		  assert(checkBattery.batteryIsOk(28, 70, 0.7f) == true);
+		  assert(checkBattery.batteryIsOk(50, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(-1, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(123, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(25, 15, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(25, 25, 0.9f) == false);
+		 
         System.out.println("Some more tests needed");
-    }
 }
