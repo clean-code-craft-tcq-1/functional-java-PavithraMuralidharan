c89e8f103320b5763658a759b6b69dd550193976
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-04-08 17:31:51 +0530

    build issue

--- vitals/Main.java
+++ vitals/Main.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		  assert(checkBattery.batteryIsOk(28, 70, 0.7f,"F") == true);
-		  assert(checkBattery.batteryIsOk(50, 85, 0.0f,"C") == false);
-		  assert(checkBattery.batteryIsOk(-1, 85, 0.0f,"C") == false);
-		  assert(checkBattery.batteryIsOk(123, 85, 0.0f,"F") == false);
-		  assert(checkBattery.batteryIsOk(25, 15, 0.0f,"F") == false);
-		  assert(checkBattery.batteryIsOk(25, 25, 0.9f,"C") == false);
+		  assert(checkBattery.batteryIsOk(28, 70, 0.7f) == true);
+		  assert(checkBattery.batteryIsOk(50, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(-1, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(123, 85, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(25, 15, 0.0f) == false);
+		  assert(checkBattery.batteryIsOk(25, 25, 0.9f) == false);
 		 
       System.out.println("Some more tests needed");
   }
