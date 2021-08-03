3dca9326e7f504733b4b5db480aa8bfcd2c6bdcf
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-08-03 13:45:27 +0530

    Update Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -3,8 +3,6 @@ package extensions;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		
 		  assert(checkBattery.batteryIsOk(28, 70, 0.7f) == true);
 		  assert(checkBattery.batteryIsOk(50, 85, 0.0f) == false);
 		  assert(checkBattery.batteryIsOk(-1, 85, 0.0f) == false);
